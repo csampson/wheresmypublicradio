bcef4879a19067fedbbcf3ec045c81645211fc71
Cameron Sampson <vaerros@gmail.com>
2013-10-16 20:28:38 -0500

    app.js indentation O-o

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,36 @@
 var wheresMyNpr = angular.module('wheresMyNpr', []);
 
 wheresMyNpr.controller('StationFinderCtrl', function($scope, $http, $q) {
-	$scope.getLocation = function() {
-		var deferred = $q.defer();
+  $scope.getLocation = function() {
+    var deferred = $q.defer();
 
-		navigator.geolocation.getCurrentPosition(
-			function(geoposition) {
-				$scope.$apply(function() {
-					deferred.resolve({ latitude: geoposition.coords.latitude, longitude: geoposition.coords.longitude });
-				});
-			},
-			function(error) {
-				deferred.resolve({ error: error });
-			}
-		);
+    navigator.geolocation.getCurrentPosition(
+      function(geoposition) {
+        $scope.$apply(function() {
+          deferred.resolve({ latitude: geoposition.coords.latitude, longitude: geoposition.coords.longitude });
+        });
+      },
+      function(error) {
+        deferred.resolve({ error: error });
+      }
+    );
 
-		return deferred.promise;
-	};
+    return deferred.promise;
+  };
 
-	$scope.findByZip = function() {
-		$http.get('/best_station', { params: { zipcode: $scope.zipcode }}).success(function(response) {
-	    $scope.bestStation = response;
-	  });
-	};
+  $scope.findByZip = function() {
+    $http.get('/best_station', { params: { zipcode: $scope.zipcode }}).success(function(response) {
+      $scope.bestStation = response;
+    });
+  };
 
-	$scope.findByLocation = function() {
-		$scope.getLocation().then(function(result) {
-			// TODO: handle errors
+  $scope.findByLocation = function() {
+    $scope.getLocation().then(function(result) {
+      // TODO: handle errors
 
-			$http.get('/best_station', { params: result}).success(function(response) {
-		    $scope.bestStation = response;
-		  });
-		});
-	};
+      $http.get('/best_station', { params: result}).success(function(response) {
+        $scope.bestStation = response;
+      });
+    });
+  };
 });
