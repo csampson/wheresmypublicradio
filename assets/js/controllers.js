645a6931932883961349aa17fd06e652e24d71d4
Cameron Sampson <vaerros@gmail.com>
2014-01-21 00:47:13 -0600

    Search form required fields and loading indicator

--- assets/js/controllers.js
+++ assets/js/controllers.js
@@ -1,15 +1,21 @@
 angular.module('wheresMyNpr.controllers', [])
   .controller('StationFinderCtrl', ['$scope', '$http', 'geolocation', function($scope, $http, geolocation) {
     $scope.findByZip = function() {
+      $scope.loading = true;
+
       $http.get('/best_station', { params: { zipcode: $scope.zipcode }}).success(function(response) {
         $scope.bestStation = response;
+        $scope.loading = false;
       });
     };
 
     $scope.findByLocation = function() {
+      $scope.loading = true;
+
       geolocation.getPosition().then(function(result) {
         $http.get('/best_station', { params: result}).success(function(response) {
           $scope.bestStation = response;
+          $scope.loading = false;
         });
       });
     };
