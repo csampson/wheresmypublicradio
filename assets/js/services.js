ee64afe49cd17d687582c9bd66e66a0c1a5a6230
Cameron Sampson <vaerros@gmail.com>
2014-03-16 21:04:01 -0500

    Existing factories are now services

--- assets/js/services.js
+++ assets/js/services.js
@@ -1,61 +1,55 @@
 angular.module('app.services', [])
-  .factory('geolocation', ['$q', function($q) {
-    return {
-      getPosition: function() {
-        var deferred = $q.defer();
+  .service('geolocation', ['$q', function($q) {
+    this.getPosition = function() {
+      var deferred = $q.defer();
 
-        navigator.geolocation.getCurrentPosition(
-          function(geoposition) {
-            deferred.resolve({ latitude: geoposition.coords.latitude, longitude: geoposition.coords.longitude });
-          },
-          function(error) {
-            deferred.resolve({ error: error });
-          },
-          {
-            timeout: 15000
-          }
-        );
+      navigator.geolocation.getCurrentPosition(
+        function(geoposition) {
+          deferred.resolve({ latitude: geoposition.coords.latitude, longitude: geoposition.coords.longitude });
+        },
+        function(error) {
+          deferred.resolve({ error: error });
+        },
+        {
+          timeout: 15000
+        }
+      );
 
-        return deferred.promise;
-      }
+      return deferred.promise;
     };
   }])
-  .factory('geocoder', ['$q', function($q) {
+  .service('geocoder', ['$q', function($q) {
     var geocoder = new google.maps.Geocoder();
 
-    return {
-      geocode: function(location) {
-        var deferred = $q.defer();
+    this.geocode = function(location) {
+      var deferred = $q.defer();
 
-        geocoder.geocode({ 'address': location }, function(results, status) {
-          if (status === google.maps.GeocoderStatus.OK) {
-            var geolocation = results[0].geometry.location;
-            deferred.resolve({ longitude: geolocation.lng(), latitude: geolocation.lat() });
-          }
-          else {
-            deferred.resolve({ error: status });
-          }
-        });
+      geocoder.geocode({ 'address': location }, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          var geolocation = results[0].geometry.location;
+          deferred.resolve({ longitude: geolocation.lng(), latitude: geolocation.lat() });
+        }
+        else {
+          deferred.resolve({ error: status });
+        }
+      });
 
-        return deferred.promise;
-      }
+      return deferred.promise;
     };
   }])
-  .factory('stationFinder', ['$q', '$http', function($q, $http) {
-    return {
-      findBestStation: function(geolocation) {
-        var deferred = $q.defer();
+  .service('stationFinder', ['$q', '$http', function($q, $http) {
+    this.findBestStation = function(geolocation) {
+      var deferred = $q.defer();
 
-        $http.get('/best_station', {params: geolocation}).success(function(response) {
-          if(!response) {
-            deferred.resolve({ error: "We couldn't find any member stations in your area." });
-          }
-          else {
-            deferred.resolve(response);
-          }
-        });
+      $http.get('/best_station', {params: geolocation}).success(function(response) {
+        if(!response) {
+          deferred.resolve({ error: "We couldn't find any member stations in your area." });
+        }
+        else {
+          deferred.resolve(response);
+        }
+      });
 
-        return deferred.promise;
-      }
+      return deferred.promise;
     };
   }]);
