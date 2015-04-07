82323be91e7049c79524c33d182850c9d4fae9a0
Cameron Sampson <vaerros@gmail.com>
2015-04-06 21:41:43 -0500

    Gulp additions, stylus over sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,30 +2,70 @@
 
 var source = require('vinyl-source-stream');
 
+var del = require('del');
+
 var gulp     = require('gulp'),
     gulpUtil = require('gulp-util');
     
 var browserify = require('browserify'),
     watchify   = require('watchify');
 
+var stylus = require('gulp-stylus'),
+    nib    = require('nib');
+
 var config = {
-  browserify: {
-    debug: gulpUtil.env.type !== 'production'
-  }
+  debug: gulpUtil.env.type !== 'production'
 };
 
-var bundler = watchify(browserify('./assets/js/app.js', config.browserify));
+var scriptBundler = browserify('./assets/js/app.js', config.debug);
 
-function bundle() {
-  return bundler.bundle()
-    .on('error', gulpUtil.log.bind(gulpUtil, 'Browserify Error'))
+function bundleScripts(options) {
+  var bundler = options.watch ? watchify(scriptBundler) : scriptBundler,
+      stream  = bundler.bundle();
+
+  if (options.watch) {
+    bundler.on('update', function() {
+      console.log('Re-bundling scripts...'); // TODO: built-in way to handle this logging..?
+      bundleScripts({ watch: false });
+    });
+  }
+
+  if (config.debug) {
+    stream.on('error', gulpUtil.log.bind(gulpUtil, 'Browserify Error'));
+  }
+
+  return stream
     .pipe(source('app.js'))
     .pipe(gulp.dest('./public/js'));
 }
 
-gulp.task('build', bundle);
-
-bundler.on('update', function() {
-  gulpUtil.log('Re-bundling...');
-  bundle();
+gulp.task('clean-css', function() {
+  return del('./static/**/*.css');
 });
+
+gulp.task('bundle-css', ['clean-css'], function () {
+  gulp.task('compile-css', function() {
+    return gulp.src('./assets/css/app.styl')
+      .pipe(stylus({ use: [nib()] }))
+      .pipe(gulp.dest('./public/css/'));
+  });
+});
+
+gulp.task('watch-css', ['bundle-css'], function() {
+  gulp.watch('./assets/**/*.styl', ['bundle-css']);
+});
+
+gulp.task('clean-js', function() {
+  return del('./static/**/*.js');
+});
+
+gulp.task('bundle-js', ['clean-js'], function() {
+  bundleScripts({ watch: false });
+});
+
+gulp.task('watch-js', ['bundle-js'], function() {
+  bundleScripts({ watch: true });
+});
+
+gulp.task('bundle', ['bundle-css', 'bundle-js']);
+gulp.task('watch', ['watch-css', 'watch-js']);
